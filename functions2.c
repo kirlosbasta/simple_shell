ae7727c81d833e5045131aa2556f188aac497ac5
kirlos basta <kirlosbasta@gmail.com>
2023-11-06 20:43:44 +0200

    Add _strdup.c

--- functions2.c
+++ functions2.c
@@ -58,3 +58,38 @@ void printenv(char **env)
 		write(STDOUT_FILENO, "\n", 2);
 	}
 }
+
+/**
+ * _strdup - return a copy of a string to newly allocated space in memory
+ * @str: pointer to string
+ *
+ * Return: Pointer of the duplicated string in success
+ * and NULL if insufficient memory was available
+ */
+
+char *_strdup(char *str)
+{
+	int i, len = 0;
+	char *s;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	s = malloc(sizeof(char) * (len + 1));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != 0; i++)
+	{
+		s[i] = str[i];
+	}
+	s[i] = '\0';
+	return (s);
+}
